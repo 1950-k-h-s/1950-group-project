d3f3b3310f4aa3b585fa4c82eb2dc59f48a977cb
Kitty <kittyamrita@hushmail.com>
2019-07-06 11:37:06 -0700

    style footer

--- Sasan's prototype/footer.php
+++ Sasan's prototype/footer.php
@@ -1,3 +1,6 @@
 <footer>
-        <p>&copy; Company 2017</p>
+    <ul class="list-inline d-flex justify-content-around">
+        <li class="list-inline-item"><a href="#">Top of Page</a></li>
+        <li class="list-inline-item"><a href="#">Validate HTML</a></li>
+    </ul>
     </footer>
