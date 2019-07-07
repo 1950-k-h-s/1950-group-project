4016dbb42544dcf1f8200eae7a35f066679cb09d
sasan <52471087+sasanp@users.noreply.github.com>
2019-07-07 12:07:33 -0700

    button added

---
+++ Sasan's prototype/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <ul class="list-inline d-flex justify-content-around">
+        <li class="list-inline-item"><a href="#">Top of Page</a></li>
+        <li class="list-inline-item"><a href="#">Validate HTML</a></li>
+    </ul>
+</footer>
